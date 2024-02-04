142bc7a0b13a29a2616d619fcf8474b297406556
TukangM <bombots23@gmail.com>
2024-02-04 02:12:39 +0000

    add rm -f "/data/local/pmmp/start.sh" on line 21 to remove while updating module

--- customize.sh
+++ customize.sh
@@ -15,9 +15,10 @@ ui_print "                                   Made with AI generated chat.openai.
 ui_print "                      https://chat.openai.com/share/8f541b0c-947d-438a-a7c1-18ac00054a93                          "
 
 # Menghapus file dan folder jika sudah ada
-ui_print "Removing existing files and folders"
-rm -rf "/data/local/pmmp/PocketMine-MP.phar"
+ui_print "Removing existing files such a PocketMine-MP.phar, bin, start.sh"
+rm -f "/data/local/pmmp/PocketMine-MP.phar"
 rm -rf "/data/local/pmmp/bin"
+rm -f "/data/local/pmmp/start.sh"
 
 # Membuat struktur direktori
 ui_print "Creating directory for server PocketMine-MP on /data/local/pmmp"
@@ -35,11 +36,11 @@ chmod +x -R "/data/local/pmmp"
 ui_print "Giving chmod permission for start.sh"
 chmod +x "/data/local/pmmp/start.sh"
 
-# (Opsional) Menyalin start-pmmp
+# Menyalin start-pmmp
 ui_print "Copy start-pmmp shell script to /system/bin"
 # cp "$MODDIR/start-pmmp" "/system/bin" suggest by @atrate in https://github.com/Magisk-Modules-Alt-Repo/submission/issues/215?notification_referrer_id=NT_kwDOBXOwbrM5MDc0NjgwODkyOjkxNDY3ODg2
 
-# (Opsional) Berikan izin eksekusi pada start-pmmp
+# Berikan izin eksekusi pada start-pmmp
 ui_print "Giving chmod permission for start-pmmp"
 chmod +x "$MODDIR/system/start-pmmp"
 
